6143b3c671719e67c9b5b7c99c5075c5b83c7848
Angel Sosa <gabrielo.09.angel@gmail.com>
2023-01-16 23:10:02 -0500

    Backend antes de usar swagger

---
+++ src/main/java/com/example/demo/domain/repository/PurchaseRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain.repository;
+
+import com.example.demo.domain.Purchase;
+import com.example.demo.domain.PurchaseItem;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
